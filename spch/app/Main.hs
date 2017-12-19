d9686e5bb88ac370347731c7e6a4571f2c4e8651
Sasaki <sasaki_ryosuke@nuco.co.jp>
2017-12-19 10:06:27 +0000

    http処理追加

--- spch/app/Main.hs
+++ spch/app/Main.hs
@@ -1,6 +1,18 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
-import Lib
+import Network.HTTP.Types        (status200)
+import Network.HTTP.Types.Header (hContentType)
+import Network.Wai               (Application, responseLBS)
+import Network.Wai.Handler.Warp  (run)
+    
+main :: IO()
+main = do
+  let port = 3000
+  putStrLn $ "Listening on port " ++ show port
+  run port app
 
-main :: IO ()
-main = someFunc
+app :: Application
+app req f =
+  f $ responseLBS status200 [(hContentType, "text/plain")] "Hello world!"
